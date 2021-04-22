7ddaf9b086fafcfe98c882053435c605e701900c
vadim-ch-17 <vadimchechenya@gmail.com>
2021-04-22 09:56:45 +0300

    Add form&anim

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -50,3 +50,22 @@ $('a[href^="#"').on('click', function() {
     return false;
 });
 	
+
+// Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+    var forms = document.getElementsByClassName('needs-validation');
+    // Зацикливайтесь на них и предотвращайте подчинение
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
